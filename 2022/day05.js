2f8a7c04421142eac976280763f8234c852fa18b
ropaolle <ropaolle@gmail.com>
2022-12-05 11:47:35 +0100

    2022 day 5

--- 2022/day05.js
+++ 2022/day05.js
@@ -1,13 +1,51 @@
-const regEx = /([a-z-]+)|([0-9]+)/g;
+const parse = (input) => {
+  const data = input.split('\n').reduce(
+    (acc, line) => {
+      if (line.length > 0) {
+        if (line.startsWith('move')) {
+          acc.instructions.push(line.match(/\d+/g).map(Number));
+        } else if (line.indexOf('[') > -1) {
+          for (let i = 0; i < line.length / 4; i++) {
+            const chr = line[i * 4 + 1];
+            if (i in acc.stacks === false) {
+              acc.stacks.push([]);
+            }
+            if (chr !== ' ') {
+              acc.stacks[i].push(chr);
+            }
+          }
+        }
+      }
+      return acc;
+    },
+    { stacks: [], instructions: [] }
+  );
 
-const parse = (input) => input.split('\n').map((line) => line);
+  data.stacks = data.stacks.map((a) => a.reverse());
+
+  return data;
+};
 
 const partOne = (input) => {
-  const data = parse(input);
+  const { stacks, instructions } = parse(input);
+
+  for (const [count, from, to] of instructions) {
+    for (let i = 0; i < count; i++) {
+      stacks[to - 1].push(stacks[from - 1].pop());
+    }
+  }
+
+  return stacks.map((v) => v.pop()).join('');
 };
 
 const partTwo = (input) => {
-  const data = parse(input);
+  const { stacks, instructions } = parse(input);
+
+  for (const [count, from, to] of instructions) {
+    stacks[to - 1] = stacks[to - 1].concat(stacks[from - 1].splice(-count));
+  }
+
+  return stacks.map((v) => v.pop()).join('');
 };
 
 exports.partOne = partOne;
