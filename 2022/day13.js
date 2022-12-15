5826d0b9ca80087127caf30b0093e200cd87715d
Olof Sjögren <ropaolle@gmail.com>
2022-12-15 15:21:43 +0100

    2022 day 13

--- 2022/day13.js
+++ 2022/day13.js
@@ -1,46 +1,69 @@
 const parse = (input) => input.split('\n\n').map((group) => group.split('\n').map(JSON.parse));
 
-const compare = (l, r) => {
-  // console.log('l,r', l, r);
-  if (typeof l === 'number') {
-    l = [l];
-  }
-  if (typeof r === 'number') {
-    r = [r];
-  }
-
-  if (l.length === 0) {
-    return true;
-  }
-
-  let x = false;
-  for (let i = 0; i < l.length; i++) {
-    if (i < r.length) {
-      const status = compare(l[i], r[i]);
-      if (status) {
-      }
-      console.log('x', x);
+const compare = ([left, right]) => {
+  if ([left, right].every(Number.isInteger)) {
+    if (left < right) {
+      return true;
     }
+    if (left > right) {
+      return false;
+    }
+    return;
   }
 
-  return x;
+  if ([left, right].every(Array.isArray)) {
+    for (let i = 0; i < Math.min(left.length, right.length); i++) {
+      const res = compare([left[i], right[i]]);
+      if (res != null) {
+        return res;
+      }
+    }
+
+    return compare([left.length, right.length]);
+  }
+
+  return compare([[left].flat(), [right].flat()]);
 };
 
 const partOne = (input) => {
   const data = parse(input);
-  // console.log('data', data);
+  let sum = 0;
 
-  let t = 0;
-
-  for (let [l, r] of data) {
-    compare(l, r);
-
-    // console.log('a', l);
+  for (let i = 0; i < data.length; i++) {
+    if (compare(data[i])) {
+      sum += i + 1;
+    }
   }
+
+  return sum;
 };
 
 const partTwo = (input) => {
   const data = parse(input);
+
+  const divider1 = [[2]];
+  const divider2 = [[6]];
+  let messages = [divider1, divider2];
+
+  for (const msg of data) {
+    messages.push(...msg);
+  }
+
+  messages = messages.sort((a, b) => {
+    if (compare([a, b]) === false) {
+      return 1;
+    }
+    if (compare([a, b]) === true) {
+      return -1;
+    }
+
+    return 0;
+  });
+
+  return (
+    (messages.findIndex((v) => v === divider1) + 1) *
+    (messages.findIndex((v) => v === divider2) + 1)
+  );
 };
 
 exports.partOne = partOne;
